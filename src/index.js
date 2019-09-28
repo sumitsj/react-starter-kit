85723f44f13455977768580ca0744222248d4b60
Sumit Jadhav <summit.jadhav@gmail.com>
2019-09-28 20:48:16 +0530

    Added CSS Reset

--- src/index.js
+++ src/index.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import HelloWorld from './components/HelloWorld';
+import './assets/styles/index.scss';
 
 ReactDOM.render(<HelloWorld />, document.getElementById('root'));
